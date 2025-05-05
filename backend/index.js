e9ab0a88ab4c0b3402987468596063130cd5fc56
thrustyvector <aryan08dhariwal@gmail.com>
2025-05-05 17:25:27 +0530

    Initial Folder Setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+// import express from "express"
+// import dotenv from "dotenv"
+// import cookieParser from "cookie-parser"
+// import cors from "cors"
+
+// dotenv.config()
+// const app = express()
+// const PORT = process.env.PORT
+// app.use(express.json())
+// app.use(cookieParser())
+// app.use(cors())
+
+// app.listen(PORT,()=>{
+//     connectDb()
+// })
