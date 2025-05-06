629f8096a80253441cd20c864994d26de50fb5b0
Hustler004 <agamagarwal004@gmail.com>
2025-05-06 11:51:28 +0530

    lessgo

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectDb } from "./src/lib/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./src/lib/db.js";
 
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT
-app.use(express.json())
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
 
-
-
-app.listen(PORT,()=>{
-    console.log("Server Runnin at ", PORT)
-    connectDb()
-})
+app.listen(PORT, () => {
+  console.log("Server Runnin at ", PORT);
+  connectDb();
+  //sfds
+});
